a65673a0bd8609da49bb1a87bd03740dbc5b44e1
zjutszl <zjutszl@126.com>
2017-10-13 18:00:53 +0800

    简化代码，js和html文件分离

---
+++ public/siteList.js
@@ -0,0 +1,61 @@
+var siteList= [
+    {
+        title:'NodeJS学习资料',
+        content:[{
+            name:'《Node.js 包教不包会》 -- by alsotang',
+            site:'https://github.com/alsotang/node-lessons'
+        },
+        {
+            name:'深入浅出Node.js',
+            site:'https://book.douban.com/subject/25768396/'
+        }]
+    },
+    {   
+        title:'Javascript学习资料',
+        content:[{
+            name:'笑来老师推荐学习书：Eloquent JavaScript',
+            site:'http://eloquentjavascript.net/index.html'
+        },
+        {
+            name:'《Javascript权威指南》(淘宝前端团队翻译)',
+            site:''
+        },
+        {
+            name:'JavaScript编程入门 · 新生大学',
+            site:'http://js.xinshengdaxue.com/'
+        }
+    ]
+    },
+    {   
+        title:'Linux学习资料',
+        content:[{
+            name:'鸟哥的 Linux 私房菜 -- 鸟哥的 Linux 私房菜 首页',
+            site:'http://linux.vbird.org/'
+        },
+        {
+            name:' 网路基础',
+            site:'http://www.study-area.org/network/network.htm'
+        },
+        {
+            name:'电脑基础',
+            site:'http://www.study-area.org/compu/compu.htm'
+        }
+    ]
+    },
+    {   
+        title:'工具篇',
+        content:[{
+            name:'Cmd Markdown编辑阅读器(在线)',
+            site:'https://www.zybuluo.com/mdeditor'
+        },
+        {
+            name:'简繁转换--油猴脚本',
+            site:'https://userscripts-mirror.org/scripts/show/150110'
+        },
+        {
+            name:'《快速搭建 Node.js 开发环境以及加速 npm》',
+            site:'http://fengmk2.com/blog/2014/03/node-env-and-faster-npm.html'
+        }
+    ]
+    }
+]
